55a662f288e321a43b39aeb3695052f3957a54bb
JacoFaberSA <faber.jaco@hotmail.com>
2022-10-18 09:53:02 +0200

    Improve 1.5 step5

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -6,16 +6,16 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   );
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </div>
   );
 }
